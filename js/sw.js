fe218a65ee5c1acec4b6bf6ea380fbf3605a55b1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-08-29 23:16:29 +0000

    Deploy r3nya/r3nya.github.io to github.com/r3nya/r3nya.github.io.git:master

---
+++ js/sw.js
@@ -0,0 +1,1 @@
+importScripts("/js/sw-toolbox.js"),toolbox.options.debug=!0,toolbox.precache(["/index.html","/css/style.css","/fonts/fontawesome-webfont.woff","/fonts/fontawesome-webfont.woff2"]),toolbox.router.get("/",toolbox.fastest);
