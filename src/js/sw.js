29fa26d237d73c94c25a12c0340bfc84f073cb85
Andrew Makarov <me@r3nya.ru>
2019-02-16 17:54:39 +0300

    Use Karla font from NPM

--- src/js/sw.js
+++ src/js/sw.js
@@ -5,6 +5,10 @@ toolbox.precache([
   '/index.html',
   '/css/style.css',
   '/fonts/fontello.woff',
-  '/fonts/fontello.woff2'
+  '/fonts/fontello.woff2',
+  '/fonts/karla-latin-400.woff',
+  '/fonts/karla-latin-400.woff2',
+  '/fonts/karla-latin-700.woff',
+  '/fonts/karla-latin-700.woff2',
 ]);
 toolbox.router.get('/', toolbox.fastest);
