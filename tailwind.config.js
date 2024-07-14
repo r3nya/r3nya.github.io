f1109a553a1e7e95c99702e234bce9cb0b7c0f58
Andrew M <me@r3nya.ru>
2024-07-14 17:33:30 +0200

    Add dark theme (#662)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.pug"],
+  darkMode: 'media',
   theme: {
     extend: {
       colors: {
-        'palette-1': '#eceff1',
-        'palette-2': '#222',
-        'palette-3': '#757575',
-        'palette-4': '#e1e1e1',
-        'palette-5': '#00796b',
-        'palette-6': '#4db6ac',
+        'palette-1': {
+          light: '#eceff1',
+          dark: '#121212',
+        },
+        'palette-2': {
+          light: '#222',
+          dark: '#e0e0e0',
+        },
+        'palette-3': {
+          light: '#757575',
+          dark: '#9e9e9e',
+        },
+        'palette-4': {
+          light: '#e1e1e1',
+          dark: '#333333',
+        },
+        'palette-5': {
+          light: '#00796b',
+          dark: '#4db6ac',
+        },
+        'palette-6': {
+          light: '#4db6ac',
+          dark: '#00796b',
+        },
       },
       fontFamily: {
         'karla': ['Karla', 'sans-serif'],
@@ -24,4 +43,3 @@ module.exports = {
   ],
   plugins: [],
 }
-
