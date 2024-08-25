a444195a7689bae2c39cdaeb677336517fd75416
Andrew Makarov <me@r3nya.ru>
2024-08-25 22:12:48 +0200

    Update color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,45 +1,45 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.pug"],
-  darkMode: 'media',
+  darkMode: "media",
   theme: {
     extend: {
       colors: {
-        'palette-1': {
-          light: '#eceff1',
-          dark: '#121212',
+        "palette-1": {
+          light: "#ffffff", // Light background
+          dark: "#0d1117", // Dark background
         },
-        'palette-2': {
-          light: '#222',
-          dark: '#e0e0e0',
+        "palette-2": {
+          light: "#24292e", // Light text
+          dark: "#c9d1d9", // Dark text
         },
-        'palette-3': {
-          light: '#757575',
-          dark: '#9e9e9e',
+        "palette-3": {
+          light: "#586069", // Light secondary text
+          dark: "#8b949e", // Dark secondary text
         },
-        'palette-4': {
-          light: '#e1e1e1',
-          dark: '#333333',
+        "palette-4": {
+          light: "#f6f8fa", // Light secondary background
+          dark: "#161b22", // Dark secondary background
         },
-        'palette-5': {
-          light: '#00796b',
-          dark: '#4db6ac',
+        "palette-5": {
+          light: "#0366d6", // Light blue
+          dark: "#58a6ff", // Dark blue
         },
-        'palette-6': {
-          light: '#4db6ac',
-          dark: '#00796b',
+        "palette-6": {
+          light: "#6f42c1", // Light purple
+          dark: "#bc8cff", // Dark purple
         },
       },
       fontFamily: {
-        'karla': ['Karla', 'sans-serif'],
+        karla: ["Karla", "sans-serif"],
       },
     },
   },
   safelist: [
-    'icon-twitter',
-    'icon-github',
-    'icon-linkedin',
-    'icon-paper-plane',
+    "icon-twitter",
+    "icon-github",
+    "icon-linkedin",
+    "icon-paper-plane",
   ],
   plugins: [],
-}
+};
