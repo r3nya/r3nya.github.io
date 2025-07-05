370897a39f633c14d3042382b1a186204859741e
Andrew M. <me@r3nya.ru>
2025-07-05 17:59:16 +0200

    Pug → Njk (#733)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,56 +1,56 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.pug"],
-  darkMode: "media",
+  content: ['./src/*.njk'],
+  darkMode: 'media',
   theme: {
     extend: {
       colors: {
-        "palette-1": {
-          light: "#ffffff", // Light background
-          dark: "#0d1117", // Dark background
+        'palette-1': {
+          light: '#ffffff', // Light background
+          dark: '#0d1117', // Dark background
         },
-        "palette-2": {
-          light: "#24292e", // Light text
-          dark: "#c9d1d9", // Dark text
+        'palette-2': {
+          light: '#24292e', // Light text
+          dark: '#c9d1d9', // Dark text
         },
-        "palette-3": {
-          light: "#586069", // Light secondary text
-          dark: "#8b949e", // Dark secondary text
+        'palette-3': {
+          light: '#586069', // Light secondary text
+          dark: '#8b949e', // Dark secondary text
         },
-        "palette-4": {
-          light: "#f6f8fa", // Light secondary background
-          dark: "#161b22", // Dark secondary background
+        'palette-4': {
+          light: '#f6f8fa', // Light secondary background
+          dark: '#161b22', // Dark secondary background
         },
-        "palette-5": {
-          light: "#0366d6", // Light blue
-          dark: "#58a6ff", // Dark blue
+        'palette-5': {
+          light: '#0366d6', // Light blue
+          dark: '#58a6ff', // Dark blue
         },
-        "palette-6": {
-          light: "#6f42c1", // Light purple
-          dark: "#bc8cff", // Dark purple
+        'palette-6': {
+          light: '#6f42c1', // Light purple
+          dark: '#bc8cff', // Dark purple
         },
-        "gradient-from": {
-          light: "#FCB595",
-          dark: "#B58366",
+        'gradient-from': {
+          light: '#FCB595',
+          dark: '#B58366',
         },
-        "gradient-via": {
-          light: "#9477F1",
-          dark: "#6A55A8",
+        'gradient-via': {
+          light: '#9477F1',
+          dark: '#6A55A8',
         },
-        "gradient-to": {
-          light: "#5BAEEB",
-          dark: "#3A7AAE",
+        'gradient-to': {
+          light: '#5BAEEB',
+          dark: '#3A7AAE',
         },
       },
       fontFamily: {
         monocode: [
-          "ui-monospace",
-          "Cascadia Code",
-          "Source Code Pro",
-          "Menlo",
-          "Consolas",
-          "DejaVu Sans Mono",
-          "monospace",
+          'ui-monospace',
+          'Cascadia Code',
+          'Source Code Pro',
+          'Menlo',
+          'Consolas',
+          'DejaVu Sans Mono',
+          'monospace',
         ],
       },
     },
