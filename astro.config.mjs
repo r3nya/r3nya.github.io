7ba8623f69c201ad1600b252ead34ee244677bb2
Andrew Makarov <me@r3nya.ru>
2025-08-08 07:21:55 +0200

    Update GA for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  site: 'https://r3nya.ru',
   srcDir: 'src',
   publicDir: 'static',
   outDir: 'dist',
