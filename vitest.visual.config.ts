22dce3fb1284c6d1c4d780752ef768fc084ade2d
Andrew Makarov <me@r3nya.ru>
2025-11-06 14:16:46 +0100

    Add `failureThreshold` for screenshots

--- vitest.visual.config.ts
+++ vitest.visual.config.ts
@@ -6,6 +6,9 @@ export default defineConfig({
     environment: 'node',
     include: ['src/test/**/*.visual.test.ts'],
     exclude: ['**/node_modules/**', '**/dist/**'],
+    snapshotOptions: {
+      failureThreshold: 0.2,
+    },
   },
   resolve: {
     alias: {
